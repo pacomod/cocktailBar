2ccc5ff173897bfe538e43dcb7a904a78e62b706
Greg <pacome.oddgmail.com>
2016-12-20 16:16:01 +0100

    add findAllByIdNotIn

--- src/main/java/fr/formation/dao/IngredientDao.java
+++ src/main/java/fr/formation/dao/IngredientDao.java
@@ -1,5 +1,7 @@
 package fr.formation.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import fr.formation.entity.Ingredient;
 @Repository
 public interface IngredientDao extends JpaRepository<Ingredient, Integer> {
 
+	List<Ingredient> findAllByIdNotIn(final List<Integer> ids);
 }
