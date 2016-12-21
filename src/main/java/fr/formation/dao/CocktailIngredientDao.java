e8d699da1dadfb339d0c9dd665e21bf31ca4c538
Greg <pacome.oddgmail.com>
2016-12-21 17:26:31 +0100

    reindent

--- src/main/java/fr/formation/dao/CocktailIngredientDao.java
+++ src/main/java/fr/formation/dao/CocktailIngredientDao.java
@@ -3,11 +3,10 @@ package fr.formation.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import fr.formation.entity.CocktailIngredient;
 
-@Repository
+//@Repository
 public interface CocktailIngredientDao
 		extends JpaRepository<CocktailIngredient, Integer> {
 
